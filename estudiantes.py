78b6ff104b85495b85294b8c119d2a0eacf9f4c4
DavNat13 <natdavn@gmail.com>
2024-06-26 10:46:03 -0400

    Se crea lista, funcion agregar_estudiante, menu, eliminar_producto

--- estudiantes.py
+++ estudiantes.py
@@ -1,7 +1,7 @@
 lista_estudiante = []
 
-def agregar_estudiante():
-    
+def agregar_estudiante(coleccion):
+
     nombre = input("")
     edad = input("")
     curso = input("")
@@ -9,3 +9,10 @@ def agregar_estudiante():
 
     lista_estudiante.append([nombre, edad, curso, promedio])
     print("Los datos se han agregado correctamente")
+
+def menu (): 
+    print("1. Agregar un estudiante.\n2. Ver todos los estudiantes.\3. Modificar un estudiante.\n4. Eliminar un estudiante\n5. Guardar colección en un archivo\n6. Salir del programa.")
+
+def eliminar_producto ():
+    if agregar_estudiante in lista_estudiante:
+        print("")
